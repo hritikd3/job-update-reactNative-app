e2069294baca34c0b8f86bc04d37916f4bff50ae
hritikd3 <hritikdangi3@gmail.com>
2023-03-23 00:52:11 +0530

    nearby jobs card refactored

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,16 +1,17 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-
+// import RAPID_API_KEY from '@env';
 const useFetch = (endpoint, query) => {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false); 
   const [error, setError] = useState(null);
 
+  // rapidapikey= RAPID_API_KEY
   const options = {
     method: "GET",
     url: `https://jsearch.p.rapidapi.com/${endpoint}`,
     headers: {
-      "X-RapidAPI-Key": "9bec37c784msh9aedd1d46c3214dp139038jsnefa0d8ba539c ",
+      "X-RapidAPI-Key": "9bec37c784msh9aedd1d46c3214dp139038jsnefa0d8ba539c",
       "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
     },
     params: { ...query },
